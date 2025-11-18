e2eb135921c3dfe91a80e2f8c35edaa2a1498b26
unknown <pomedev21@gmail.com>
2025-11-17 18:04:52 -0800

    co

---
+++ app/k/page.js
@@ -0,0 +1,72 @@
+"use client";
+import { useState } from "react";
+
+export default function Home() {
+  const [grade, setGrade] = useState(0);
+  const [name, setName] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [school, setSchool] = useState("");
+  const [students, setStuds] = useState([]);
+  const Subm = () => {
+    const S = {
+      name,
+      lastname,
+      school,
+      grade,
+    };
+    setStuds([...students, S]);
+    setName("");
+    setGrade("");
+    setSchool("");
+    setLastname("");
+  };
+  return (
+    <div className="p-20 flex-col flex space-y-10 w-full justify-center items-center">
+      <h2 className="text-blue-300 text-2xl font-bold">Student Info Form</h2>
+
+      <input
+        type="text"
+        placeholder="Firstname"
+        value={name}
+        className="border-2 rounded-md "
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Lastname"
+        value={lastname}
+                className="border-2 rounded-md"
+        onChange={(e) => setLastname(e.target.value)}
+      />
+      <input
+        type="number"
+        placeholder="Grade"
+        value={grade}
+                className="border-2 rounded-md"
+        onChange={(e) => setGrade(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="School"
+        value={school}
+                className="border-2 rounded-md"
+        onChange={(e) => setSchool(e.target.value)}
+      />
+      <button onClick={() => Subm()} className="text-green-500 hover:cursor-pointer border-4 rounded-md border-black w-20 h-8">Submit</button>
+      <ul className="flex flex-row space-x-10">
+        {students.map((e,i)=>(
+            <div className="">
+            <li key={i}>
+                <p>Name: {e.name + " "+ e.lastname}</p>
+                <p>Grade: {e.grade}</p>
+                <p>School: {e.school}</p>
+            </li>
+            </div>
+ 
+        ))
+
+        }
+      </ul>
+    </div>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,65 +1,131 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
 
 export default function Home() {
+  const [data, setData] = useState([]);
+  const [sum, setSum] = useState(0);
+  const [max, setMax] = useState(0);
+  const [average, setAv] = useState(0);
+  let n1, n2, n3,n4,n5,n6,n7,n8, name;
+  const Sum = function (a, b) {
+    setSum(a + b);
+  };
+  const Max = function (a, b, c) {
+    if (a > b && a > c) {
+      setMax(a);
+    } else if (b > a && b > c) {
+      setMax(b);
+    } else {
+      setMax(c);
+    }
+  };
+  const Avg = function (a, b, c) {
+    setAv((a + b + c) / 3);
+  };
+  function Calc() {
+    n1 = parseFloat(document.getElementById("num1").value);
+    n2 = parseFloat(document.getElementById("num2").value);
+    n3 = parseFloat(document.getElementById("num3").value);
+    n4 = parseFloat(document.getElementById("num4").value);
+    n5 = parseFloat(document.getElementById("num5").value);
+    n6 = parseFloat(document.getElementById("num6").value);
+    n7 = parseFloat(document.getElementById("num7").value);
+    n8 = parseFloat(document.getElementById("num8").value);
+    document.getElementById("num1").value = "";
+    document.getElementById("num2").value = "";
+    document.getElementById("num3").value = "";
+   document.getElementById("num4").value= "";
+    document.getElementById("num5").value= "";
+    document.getElementById("num6").value= "";
+    document.getElementById("num7").value="";
+    document.getElementById("num8").value="";
+    name = document.getElementById("name").value;
+     document.getElementById("name").value = "";
+    data.push({ name });
+    Sum(n1, n2);
+    Max(n3, n4, n5);
+    Avg(n6, n7, n8);
+  }
+  console.log(data);
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+    <div className="w-screen h-screen bg-white flex-col justify-center items-center ">
+      <div className="w-full flex-col justify-around">
+          <input
+        type="text"
+        placeholder="Enter name"
+        className="p-2 m-2 border-2"
+        id="name"
+      />
+      <div className="flex-row justify-between h-20">
+        <input
+          type="number"
+          placeholder="Enter first number"
+          className="p-2 m-2 border-2"
+          id="num1"
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+        <input
+          type="number"
+          placeholder="Enter first number"
+          className="p-2 m-2 border-2"
+          id="num2"
+        />
+      </div>
+      <div className="flex-row justify-between h-20">
+        <input
+          type="number"
+          placeholder="Enter first number"
+          className="p-2 m-2 border-2"
+          id="num3"
+        />
+        <input
+          type="number"
+          placeholder="Enter first number"
+          className="p-2 m-2 border-2"
+          id="num4"
+        />
+        <input
+          type="number"
+          placeholder="Enter first number"
+          className="p-2 m-2 border-2"
+          id="num5"
+        />
+      </div>
+      <div className="flex-row justify-between h-20">
+        <input
+          type="number"
+          placeholder="Enter first number"
+          className="p-2 m-2 border-2"
+          id="num6"
+        />
+        <input
+          type="number"
+          placeholder="Enter first number"
+          className="p-2 m-2 border-2"
+          id="num7"
+        />
+        <input
+          type="number"
+          placeholder="Enter first number"
+          className="p-2 m-2 border-2"
+          id="num8"
+        />
+      </div>
+      </div>
+    
+
+      <button className="p-2 m-2 bg-blue-500 text-white" onClick={() => Calc()}>
+        Submit
+      </button>
+      <div className="m-4 p-4 border space-x-3">
+        <h2 className="text-xl font-bold">Results:</h2>
+        <span>Sum: {sum}</span>
+        <span>Max: {max}</span>
+        <span>Average: {average}</span>
+      </div>
+      <p>Names:</p>
+      {data.map((item, index) => (
+        <li key={index}>{item.name}</li>
+      ))}
     </div>
   );
 }
